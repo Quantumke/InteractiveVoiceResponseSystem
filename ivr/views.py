ae7e6811557a5cd0830fe9d62336e6756afb86e2
Benson Mburu <nguruben@gmail.com>
2015-10-09 18:28:31 +0300

    configured urls

--- ivr/views.py
+++ ivr/views.py
@@ -1,12 +1,14 @@
 from django_twilio.decorators import twilio_view
-from twilio.twimi import Respose
-
+from twilio.twiml import Response
 @twilio_view
 def gather_digits(request):
     twilio_response=Response()
     
     with twilio_response.gather(action='https://www.twilio.com/blog/respond/', numDigits=1) as g:
         g.say('Press one for a song, two to recieve an sms')
-        g.pause(lenght=1)
+        g.pause(length=1)
         g.say('Press one for a song, two to recieve an sms')
-return twilio_response
+        return twilio_response
+@twilio_view
+def handle_response(request):
+    
