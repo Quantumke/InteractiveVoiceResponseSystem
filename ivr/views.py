8bc9f14ff1224ed9d94400d9c68c6eeea2320f4d
Benson Mburu <nguruben@gmail.com>
2015-10-09 18:32:58 +0300

    twilio response

--- ivr/views.py
+++ ivr/views.py
@@ -11,4 +11,14 @@ def gather_digits(request):
         return twilio_response
 @twilio_view
 def handle_response(request):
+    digits =request.POST.get('Digits','')
+    twilio_response =Response()
+    
+&nbsp; if digits =='1':
+     twilio_response.play('http://bit.ly/phaltsw')
+        if digits=='2':
+            number=request.POST.get('FROM','')
+            twilio_response.say('A text message is on its way')
+            twilio_response.sms('You looking lovely today!', to=number)
+            return twilio_response
     
